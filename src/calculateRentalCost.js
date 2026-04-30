672859b349f6300502ac3c70a21affe3b9488ba4
mauroyuzo <mauroyuzof@gmail.com>
2026-04-30 02:32:27 -0300

    TASK_TRANSPORTATION_ON_VACATION

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,16 @@
-/**
- * @param {number} days
- *
- * @return {number}
- */
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+  const SHORT_PRICE = 20;
+  const LONG_PRICE = 50;
+
+  if (days >= LONG_TERM) {
+    return days * 40 - LONG_PRICE;
+  } else if (days >= SHORT_TERM) {
+    return days * 40 - SHORT_PRICE;
+  } else {
+    return days * 40;
+  }
 }
 
 module.exports = calculateRentalCost;
